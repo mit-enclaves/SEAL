25b8c10b30a56b55d14ee5286cc9af26afbbdba7
Jules Drean <drean@mit.edu>
2025-03-18 17:30:16 +0000

    Fix flags for gramine compilation

--- seal-toolchain.cmake
+++ seal-toolchain.cmake
@@ -1,5 +1,8 @@
-#set(CMAKE_SYSTEM_NAME Linux)
-#set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+
+set(CMAKE_C_FLAGS "-O3")
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}")
 
 # Set the include paths
 #set(CMAKE_C_FLAGS "-O0 -g3 -ggdb3  -Wl,-z,norelro")
